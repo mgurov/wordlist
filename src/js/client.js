b8074dfce7f301cebcb71fe8f7f6e089cf6b85ac
Mykola Gurov <ngurov@gmail.com>
2016-04-10 11:53:34 +0200

    page2->sample

--- src/js/client.js
+++ src/js/client.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 import Layout from "./Layout.js";
 import List from "./pages/List.js";
-import Page2 from "./pages/Page2.js";
+import Sample from "./pages/Sample.js";
 
 const app = document.getElementById('app');
 
+/* alternatively <Route path="/Sample" component={List}/> */
 ReactDOM.render(<Router history={hashHistory}>
     <Route path="/" component={Layout}>
-      <IndexRoute component={Page2}></IndexRoute>
-      <Route path="/page2" component={List}/>
+      <IndexRoute component={Sample}></IndexRoute>
     </Route>
   </Router>,app);
