a9f1ae57f2cccfcf5141dd8fb7b62f3c1045bdc1
Mykola Gurov <ngurov@gmail.com>
2016-04-10 13:17:30 +0200

    green color for the answered things

--- src/js/components/sample/SampleProgress.js
+++ src/js/components/sample/SampleProgress.js
@@ -4,11 +4,24 @@ import _ from "lodash";
 const Progress = React.createClass({
     
   render: function() {
+    
+    
+    const {sample, current} = this.props;
     const steps = [];
-    for (var i = 0; i < this.props.sample.length; i++) {
+    for (var i = 0; i < sample.length; i++) {
+      var symbol;
+      var color = undefined;            
+      if (i == current) {
+        symbol = '*';
+      } else if (sample[i].isAnswered()) {
+        symbol = '.';
+        color = 'green';
+      } else {
+        symbol = '.';
+      }
+      
       const onClick = _.bind(this.props.onStepSelection, this, i);
-      const symbol = (i == this.props.current) ? '*' : '.';      
-      steps.push(<span key={i} onClick={onClick} >{symbol}</span>);
+      steps.push(<span key={i} onClick={onClick} style={{color}} >{symbol}</span>);
     }
     return <div>{steps}</div>
   },  
