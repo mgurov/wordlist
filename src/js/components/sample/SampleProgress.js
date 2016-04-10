1f3504d2c04b24aa8b64ccb345f7069f9666bbfa
Mykola Gurov <ngurov@gmail.com>
2016-04-10 13:02:35 +0200

    minor refactoring

--- src/js/components/sample/SampleProgress.js
+++ src/js/components/sample/SampleProgress.js
@@ -1,19 +1,16 @@
 import React from "react";
+import _ from "lodash";
 
 const Progress = React.createClass({
     
   render: function() {
     const steps = [];
     for (var i = 0; i < this.props.sample.length; i++) {
-      const symbol = (i == this.props.current) ? '*' : '.';
-      const that = this;
-      const thatI = i;
-      const onClick = function() {
-        that.props.onStepSelection(thatI);
-      }      
+      const onClick = _.bind(this.props.onStepSelection, this, i);
+      const symbol = (i == this.props.current) ? '*' : '.';      
       steps.push(<span key={i} onClick={onClick} >{symbol}</span>);
     }
-    return <div onClick={this._onClick}>{steps}</div>
+    return <div>{steps}</div>
   },  
 });
 
