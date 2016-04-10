d05f7e572b582ee9f8ce77c9bc0e242591a4396b
Mykola Gurov <ngurov@gmail.com>
2016-04-10 12:29:21 +0200

    progress shown

--- src/js/components/sample/SampleProgress.js
+++ src/js/components/sample/SampleProgress.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 export default React.createClass({
+  
+  //size={this.props.words.length} current={this.state.position}
+  
   render: function() {
-    return <div>...</div>
+    const steps = [];
+    for (var i = 0; i < this.props.size; i++) {
+      const symbol = (i == this.props.current) ? '*' : '.';
+      
+      steps.push(<span key={i} >{symbol}</span>);
+    }
+    return <div>{steps}</div>
   }
 });
